f6bdf32dad5a654afbeda0d56b59cb030fc94004
Thomas <tvbarnette999@gmail.com>
2021-12-11 15:25:28 -0500

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,116 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BS_Utils.Utilities;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using IPALogger = IPA.Logging.Logger;
+using PCDiscordScorebot.Configuration;
+
+namespace PCDiscordScorebot
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger, IPA.Config.Config conf)
+        {
+            Instance = this;
+            Log = logger;
+            PluginConfig.Instance = conf.Generated<PluginConfig>();
+            Log.Info("Discord Scorebot config: " + PluginConfig.Instance.submitData.userId + "@" + PluginConfig.Instance.submitData.url);
+            Log.Info("PCDiscordScorebot initialized.");
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+
+            BSEvents.levelCleared += submitScore;
+
+            new GameObject("PCDiscordScorebotController").AddComponent<PCDiscordScorebotController>();
+
+        }
+
+        private string difficultyToString(BeatmapDifficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case BeatmapDifficulty.Easy:
+                    return "Easy";
+                case BeatmapDifficulty.Normal:
+                    return "Normal";
+                case BeatmapDifficulty.Hard:
+                    return "Hard";
+                case BeatmapDifficulty.Expert:
+                    return "Expert";
+                case BeatmapDifficulty.ExpertPlus:
+                    return "Expert+";
+            }
+            return "Unknown";
+        }
+
+        private void submitScore(StandardLevelScenesTransitionSetupDataSO data, LevelCompletionResults results) {
+            if (data.practiceSettings != null)
+            {
+                Log.Info("Level Completed in practice.");
+                return;
+            }
+            Score score = new Score();
+            score.userId = PluginConfig.Instance.submitData.userId;
+            score.levelId = data.difficultyBeatmap.level.levelID;
+            score.songName = data.difficultyBeatmap.level.songName;
+            score.levelAuthor = data.difficultyBeatmap.level.levelAuthorName;
+            score.songAuthor = data.difficultyBeatmap.level.songAuthorName;
+            score.difficulty = difficultyToString(data.difficultyBeatmap.difficulty);
+            score.difficultyRank = data.difficultyBeatmap.difficultyRank;
+            score.difficultyRaw = (int) data.difficultyBeatmap.difficulty;
+            score.score = results.rawScore;
+            score.modifiedScore = results.modifiedScore;
+            score.fullCombo = results.fullCombo;
+            score.goodCuts = results.goodCutsCount;
+            score.badCuts = results.badCutsCount;
+            score.missed = results.missedCount;
+            score.notGood = results.notGoodCount;
+            score.ok = results.okCount;
+            string json = JsonConvert.SerializeObject(score, Formatting.None);
+            AsyncOperation op = UnityWebRequest.Post(PluginConfig.Instance.submitData.url, json).Send();
+            Log.Info("Level Complete: " +  json);
+
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCDiscordScorebot
+{
+    class Score
+    {
+        public string levelId, songName, levelAuthor, songAuthor, difficulty;
+        public int userId, difficultyRank, difficultyRaw, score, modifiedScore, goodCuts, badCuts, missed, notGood, ok, averageCutScore, maxCutScore, maxCombo;
+        public bool fullCombo;
+        public float leftSaberDistance, leftHandDistance, rightSaberDistance, rightHandDistance, averageCutDistanceRawScore, minDirDeviation, maxDirDeviation, averageDirDeviation, minTimeDeviation, maxTimeDeviation, averageTimeDeviation;
+    }
+}
