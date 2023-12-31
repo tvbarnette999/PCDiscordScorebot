52cfc96874c09e651a6d06b90838866029a7145a
Thomas <tvbarnette999@gmail.com>
2023-12-30 23:40:14 -0500

    updates for 1.34.2

--- Score.cs
+++ Score.cs
@@ -8,9 +8,9 @@ namespace PCDiscordScorebot
 {
     class Score
     {
-        public string userId, levelId, songName, levelAuthor, songAuthor, difficulty;
-        public int difficultyRank, difficultyRaw, score, modifiedScore, goodCuts, badCuts, missed, notGood, ok, averageCutScore, maxCutScore, maxCombo;
-        public bool fullCombo;
+        public string userId, levelId, songName, levelAuthor, songAuthor, difficulty, modifiers;
+        public int difficultyRank, difficultyRaw, score, modifiedScore, multipliedScore, goodCuts, badCuts, missed, notGood, ok, averageCutScore, maxCutScore, maxCombo;
+        public bool fullCombo, withoutMods;
         public float leftSaberDistance, leftHandDistance, rightSaberDistance, rightHandDistance, averageCutDistanceRawScore, minDirDeviation, maxDirDeviation, averageDirDeviation, minTimeDeviation, maxTimeDeviation, averageTimeDeviation;
     }
 }
